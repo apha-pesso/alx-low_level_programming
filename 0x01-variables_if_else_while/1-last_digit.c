904ffac0556446ce67407862c5c7aba71c7d12ab
apha-pesso <mutiuayo99@gmail.com>
2022-03-11 22:48:35 +0000

    Corrected a comma-error, lets print the last digit now

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 		int n;
+		int last;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
@@ -19,6 +20,6 @@ int main(void)
 		else if (last == 0)
 			printf("Last digit of %d is %d and is 0\n", n, last);
 		else
-			printf("Last digit of %d is %d and is less than 6 and not 0\n" n, last);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 		return (0);
 }
