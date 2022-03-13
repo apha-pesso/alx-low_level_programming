e03b53da0f6c87283f5c626489e1e0dd6302e602
apha-pesso <mutiuayo99@gmail.com>
2022-03-13 08:15:00 +0000

    Task 8 check

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-fputs("and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", stdout);
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- Prints exactly "and that piece of art is useful"
+ * - Dora Korpor, 2015-10-19"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", 59);
+	return (1);
+}
