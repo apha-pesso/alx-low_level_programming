f45cd8d3891cedc2371a5f81e2993fb9fd4060ff
apha-pesso <mutiuayo99@gmail.com>
2022-03-18 21:15:13 +0000

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints number from 0-9
+ * but does not print 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
