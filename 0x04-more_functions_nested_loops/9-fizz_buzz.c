1b6b154b890fa950e571bfd530b56fe16364fc93
apha-pesso <mutiuayo99@gmail.com>
2022-03-19 09:20:38 +0000

    Passed the test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints number from 1 - 100 followed by new line
+ * prints fizz for multiples of 3 and buzz for multiples of 5
+ * prints fizz Buzz for multiple of 3 and 5
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		putchar(' ');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
