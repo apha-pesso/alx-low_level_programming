2d28b31cc1546dcd7b8a3af56e076c341b44a1b8
apha-pesso <mutiuayo99@gmail.com>
2022-03-20 06:40:20 +0000

    Check

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-	long int n = 612852475143, i = 3;
+	unsigned long int n = 612852475143, i = 3;
 
 	for (; i < 12057; i += 2)
 	{
 		while (n % i == 0 && n != i)
 			n /= i;
 	}
-	printf("%u\n", n);
+	printf("%lu\n", n);
 	return (0);
 }
 
