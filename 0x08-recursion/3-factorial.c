4d123745fb029644c3860643fff7009c0ec5a616
apha-pesso <mutiuayo99@gmail.com>
2022-03-30 23:43:38 +0000

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a number
+ * @n: integer for operation
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
