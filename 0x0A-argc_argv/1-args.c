bc0407dfe4268f6ac31f573973b012af4d614111
apha-pesso <mutiuayo99@gmail.com>
2022-04-02 15:17:13 +0000

    Betty wahala

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,12 +3,12 @@
 /**
  * main - this is where the code starts
  * @argc: argument count
- * @argc: list of arguments
+ * @argv: list of arguments
  * Return: 0 Success
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
