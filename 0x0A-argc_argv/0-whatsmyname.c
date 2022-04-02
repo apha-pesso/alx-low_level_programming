47e1900e62d210f7215a6f80044da0f9abfb1364
apha-pesso <mutiuayo99@gmail.com>
2022-04-02 14:40:59 +0000

    First task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - where the program starts
+ * @argc: number of arguments
+ * @argv: name list of arguments
+ * Return: new name
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Solution to argc_argv tasks
