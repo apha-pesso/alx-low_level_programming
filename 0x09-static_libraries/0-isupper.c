8f4a33e61b403c501287b1c9dc18e1df0f037e7c
apha-pesso <mutiuayo99@gmail.com>
2022-04-04 11:54:10 +0000

    check

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper- This function checks for uppercase
+ * @c: c is a letter
+ * Return: 1 if it is uppercase
+ * and returns 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
