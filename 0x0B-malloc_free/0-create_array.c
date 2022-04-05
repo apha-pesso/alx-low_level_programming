91d4905d24d4d6b7c372edd941610f14aa2d88cb
apha-pesso <mutiuayo99@gmail.com>
2022-04-05 13:15:54 +0000

    sweet malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - function to create an array and initialize to specific char
+ * @size: size of the array
+ * @c: character to be initialized
+ * Return: null if size is 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *point;
+	unsigned int i = 0;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	point = malloc(sizeof(char) * size);
+	
+	
+	while (i < size)
+	{
+		point[i] = c;
+		i++;
+	}
+
+	return (point);
+}
