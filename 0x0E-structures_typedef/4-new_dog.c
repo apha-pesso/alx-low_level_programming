145fbab7f12688975543d967438ccb01bb09b3c6
apha-pesso <mutiuayo99@gmail.com>
2022-04-13 08:50:47 +0000

    free dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+
+/**
+ * new_dog - function that creates new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+		return;
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+}
