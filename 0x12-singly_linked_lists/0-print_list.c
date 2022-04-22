e44291371184e1ddb3d5dc7f861235ce1c592c4d
apha-pesso <mutiuayo99@gmail.com>
2022-04-22 16:55:58 +0000

    print element of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * print_list - function to print all element of a list_t list
+ * @h: list to be printed
+ * Return: return the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{	
+	int i = 0;
+
+
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
