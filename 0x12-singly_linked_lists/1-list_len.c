0cc41db1bb4c7fc0b608a6ccdaea7d7c0f553c23
apha-pesso <mutiuayo99@gmail.com>
2022-04-22 17:14:02 +0000

    returns lenght of linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns number of elements
+ * @h: the linked list
+ * Return: returns the number of data
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
