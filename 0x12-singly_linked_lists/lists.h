cbc6e50961f0bcc57d7c6207c0aefd7de2e96c6c
apha-pesso <mutiuayo99@gmail.com>
2022-04-23 11:59:15 +0000

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef LIST
 #define LIST
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 
@@ -21,5 +24,8 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+
 
 #endif
