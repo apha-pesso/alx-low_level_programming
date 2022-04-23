f626e698e9c343e343a93cddb099cb56fb4ab277
apha-pesso <mutiuayo99@gmail.com>
2022-04-23 11:49:43 +0000

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ * @head: head of linked list
+ * @str: string to add to list
+ * Return: addres of head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *nnode;
+	size_t n;
+
+	nnode = malloc(sizeof(list_t));
+	if (nnode == NULL)
+		return (NULL);
+	nnode->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	nnode->len = n;
+	nnode->next = *head;
+	*head = n;
+
+	return (*head);
+}
