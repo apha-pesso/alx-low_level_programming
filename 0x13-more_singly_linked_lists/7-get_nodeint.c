84e5bb6303c8c643ff1d82e9948448e185501acf
apha-pesso <mutiuayo99@gmail.com>
2022-04-27 11:51:17 +0000

    get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 	for (i = 0; i < index; i++)
 	{
+		if (head == NULL)
+			return (NULL);
+
 		head = head->next;
 	}
 	return (head);
