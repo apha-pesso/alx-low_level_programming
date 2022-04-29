0dd7be5bc70b722795a2f5497e3cdeaa5712a823
apha-pesso <mutiuayo99@gmail.com>
2022-04-29 16:58:54 +0000

    checks

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - returns value of bit at given index
+ * @n: given number
+ * @index: index of bit
+ * Return: value of bit or -1 on error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i < 63; n >>= 1, i++)
+	{
+		if (index == i)
+			return (n & 1);
+	}
+	return (-1);
+}
