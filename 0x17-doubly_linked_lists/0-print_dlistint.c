2294bbb152592b680a04148e6cb6ccb96288fff4
apha-pesso <mutiuayo99@gmail.com>
2022-09-02 09:34:12 +0000

    Print element and return number of elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,3 +1,19 @@
 #include "lists.h"
+/**
+ * print_dlistint - Prints elements in a doubly linked list
+ * @h: list
+ * Return: Returns number of elements
+ */
 
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
 
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
