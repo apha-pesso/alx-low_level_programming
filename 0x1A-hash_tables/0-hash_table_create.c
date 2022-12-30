8b60f4839981872c92fa40e1e2389ea9c89976b6
apha-pesso <mutiuayo99@gmail.com>
2022-12-30 10:36:08 +0000

    Betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,20 @@
 #include "hash_tables.h"
-
+/**
+ *  hash_table_create- create hash table
+ *  @size: size of the table
+ *  Return: return pointer to the table
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-		return NULL;
+		return (NULL);
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t) * size);
 	if (ht->array == NULL)
-		return NULL;
+		return (NULL);
 	return (ht);
 }
