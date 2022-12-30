656926b9c1911c53f624cfe039acbfc7dbdb8d51
apha-pesso <mutiuayo99@gmail.com>
2022-12-30 11:15:07 +0000

    check

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 /**
- * hash_djb2- Create a hsash function
- * @str: string used to generate the hash value
- * Return: return hash value
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
-
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
