bf132152c459a78519e8c9dc6627cefcc7bbb9bd
apha-pesso <mutiuayo99@gmail.com>
2023-05-24 14:25:07 +0100

    Deal with indexerror

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 '''returns the perimeter of the island described in grid'''
 
+
 def island_perimeter(grid):
     """Return Island perimeter"""
     island_perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
+
+                # Check up
                 if (i == 0) or (grid[i - 1][j] == 0):
                     island_perimeter += 1
                 if (grid[i][j - 1] == 0) or (j == 0):
                     island_perimeter += 1
-                if (grid[i][j + 1] == 0) or (j == (len(grid[i]) - 1)):
+                if (j == (len(grid[i]) - 1)) or (grid[i][j + 1] == 0):
                     island_perimeter += 1
-                if (grid[i + 1][j] == 0) or (i == (len(grid) - 1)):
+                # if (grid[i + 1][j] == 0) or (i == (len(grid) - 1)):
+                if (i == (len(grid) - 1)) or (grid[i + 1][j] == 0):
                     island_perimeter += 1
     return (island_perimeter)
